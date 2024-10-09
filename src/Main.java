a0f6c299ec36ee690e8d4e1a07262b5014ba9793
Student Free <l3ra131007@gmail.com>
2024-10-09 17:46:26 +0300

    finall commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.time.LocalDate;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static String checkYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return year + " год является високосным";
+        } else {
+            return year + " год не является високосным";
         }
     }
+
+    public static String appVersion(int numberOs, int yearRelease) {
+        if (numberOs == 0) {
+            int currentYear = LocalDate.now().getYear();
+            if (yearRelease < currentYear) {
+                return "Установите облегченную версию приложения для IOS";
+            } else {
+                return "Установите обычную версию приложения для IOS";
+            }
+        } else {
+            int currentYear = LocalDate.now().getYear();
+            if (yearRelease < currentYear) {
+                return "Установите облегченную версию приложения для Android";
+            } else {
+                return "Установите обычную версию приложения для Android";
+            }
+        }
+
+    }
+
+    public static String deliveryBankCard(int deliveryDistance) {
+        if (deliveryDistance < 20) {
+            return "Потребуется дней: " + 0;
+        } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
+            return "Потребуется дней: " + 1;
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            return "Потребуется дней: " + 2;
+        } else {
+            return "Доставки нет ";
+        }
+    }
+
+    public static void main(String[] args) {
+        int year = 2000;
+        System.out.println(checkYear(year));
+        int os = 1;
+        int yearRelease = 2024;
+        System.out.println(appVersion(os, yearRelease));
+        int deliveryDistance = 95;
+        System.out.println(deliveryBankCard(deliveryDistance));
+    }
 }
+
